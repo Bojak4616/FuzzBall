7cb8e032ffc76b4afe3975ddb8faf8632f2ada29
Jared E Stroud <jaredestroud@gmail.com>
2015-10-21 21:10:23 -0400

    try/catch for scapy

--- pcapRead.py
+++ pcapRead.py
@@ -4,6 +4,12 @@
     modification and protocol specific dissection.
 
 '''
+try:
+	import sys
+	import scapy
+except ImportError as err:
+	print("[-] Cannot find package %s. Exiting", err)
+	sys.exit()
 
 __author__ = 'Jared'
 
@@ -15,3 +21,6 @@ class PRead:
 
 #TODO
 # Read/write/hexdump functionality. Probably more things I haven't thought of yet.
+
+if __name__ == "__main__":
+	print("Running pcapRead...")
