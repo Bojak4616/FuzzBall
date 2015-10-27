c27b1767672bc6fb2a828a85d37787bb0d5d9906
jastra <jaredestroud@gmail.com>
2015-10-27 11:50:54 -0400

    Added REPL functionality within a command class. This might be moved later on to its own class file.

--- Fuzzer.py
+++ Fuzzer.py
@@ -1,6 +1,42 @@
 #!/usr/bin/env python
 __author__ = 'Jared'
 
+class evalCmd:
+
+    def cmdCheck(self, commandToEvaluate):
+
+        if commandToEvaluate == "quit":
+            import sys
+            sys.exit()
+
+        elif commandToEvaluate == "help":
+            print("""
+                    load: load a pcap for operations to be performed on.
+                    parse: parse a loaded pcap.
+                    wireshark: launch modified pcap in wiresharl.
+                    help: launch this program.
+                    quit: exit the program.
+                  """)
+        
+        elif commandToEvaluate == "load":
+            print("TODO: implement loading of PCAP")
+
+        elif commandToEvaluate == "parse":
+            print("TODO: implement parsing")
+
+        elif commandToEvaluate == "wireshark":
+            print("TODO: launch wireshark with loaded pcap")
+        else:
+            print("Invalid command. Try typing help")
 
 if __name__ == "__main__":
     print("Welcome to fuzzball, shall we begin?")
+    test = evalCmd()
+    while True:
+        command = raw_input(">> ")
+        try:
+            test.cmdCheck(command)
+        except ValueError as err: 
+            print("Sorry, couldn't perform operation.")
+
+
