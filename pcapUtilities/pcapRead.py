992ad04673c64550b2a6d62515ac9688aac5f0f8
jastra <jaredestroud@gmail.com>
2015-11-03 16:42:21 -0500

    Added BSD 3 license from DPKT library (because we're using some of their code)

--- pcapUtilities/pcapRead.py
+++ pcapUtilities/pcapRead.py
@@ -1,36 +1,56 @@
 #!/usr/bin/env python
 
+'''
+Copyright (c) 2004 Dug Song <dugsong@monkey.org>
+All rights reserved, all wrongs reversed.
 
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
 
+1. Redistributions of source code must retain the above copyright
+notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+notice, this list of conditions and the following disclaimer in the
+documentation and/or other materials provided with the distribution.
+3. The names of the authors and copyright holders may not be used to
+endorse or promote products derived from this software without
+specific prior written permission.
 
-
-
-
-
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 '''
+
+try:
+    import socket
+    import datetime
+    import sys
+    import dpkt
+    from scapy import *
+    from pcapfile import savefile
+except ImportError as err:
+    print("[-] Cannot find package: " + str(err))
+    sys.exit()
+
+class PRead:
+
+    '''
     Name: pcapRead
     Purpose: Read in pcap file for future
     modification and protocol specific dissection.
 
     Some methods were taken from the dpkt Python module example Github repo.
     Source: https://raw.githubusercontent.com/kbandla/dpkt/master/examples/print_packets.py
-'''
-try:
-        import socket
-        import datetime
-	import sys
-        import dpkt
-        from scapy import *
-        from pcapfile import savefile
-except ImportError as err:
-    print("[-] Cannot find package: " + str(err))
-    sys.exit()
-
-__author__ = 'Jared'
-
-class PRead:
-
+    '''
     '''
         Name: __init__
         Purpose: constructor
